28ff92bc1e2618aaa03280dda9184e75d1793d45
ZenMoose <mostafa.habib@zenbusiness.com>
2023-01-07 00:21:17 -0500

    basic setup for backend, db, orm, queries, mutation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { gql, useQuery, useMutation } from '@apollo/client'
 import './App.css';
 
+const GET_POSTS = gql`
+  query GetPosts {
+    getPosts {
+      id
+      title
+    }
+  }
+`
+
+const SEARCH_POSTS = gql`
+  query searchPosts ($searchTerm: String) {
+    searchPosts(term: $searchTerm) {
+      id
+      title
+    }
+  }
+`
+
+const ADD_POST = gql`
+  mutation Post ($post: String!) {
+    post(title: $post) {
+      title
+    }
+  }
+`
+
+
 function App() {
+
+  const [input, setInput] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+  const [postList, setPostsList] = useState([]);
+  const [searchList, setSearchList] = useState([]);
+
+  const { data: { getPosts } = [] } = useQuery(GET_POSTS);
+  const { data: { searchPosts } = [] } = useQuery(SEARCH_POSTS, {
+    variables: { searchTerm: searchInput }
+  })
+  const [addPost] = useMutation(ADD_POST);
+
+  useEffect(() => {
+    setPostsList(getPosts)
+  }, [getPosts, postList])
+
+  const handleSubmit = event => {
+    addPost({ variables: { post: input } });
+  }
+
+  const handleSearch = event => {
+    event.preventDefault();
+    setSearchList(searchPosts)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Add image</h1>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Add Post</p>
+            <input
+              onChange={e => (setInput(e.target.value))}
+              onSubmit={e => handleSubmit(input)}
+            />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+      {postList?.length && postList.map(x => (
+        <div key={x.id}>
+          <span>{x.id}</span>
+          <span>{x.title}</span>
+        </div>
+      ))
+      }
+      <h1>Search Images</h1>
+      <form onSubmit={handleSearch}>
+        <fieldset>
+          <label>
+            <p>Search</p>
+            <input
+              onChange={e => (setSearchInput(e.target.value))}
+              onSubmit={e => handleSearch(input)}
+            />
+          </label>
+        </fieldset>
+        <button type="submit">Search</button>
+      </form>
+      {searchList?.length > 0 && searchList.map(x => (
+        <div key={x.id}>
+          <span>{x.title}</span>
+        </div>
+      ))
+      }
     </div>
   );
 }
---
+++ src/server/index.js
@@ -0,0 +1,66 @@
+const Fastify = require('fastify')
+const cors = require('@fastify/cors')
+const mercurius = require('mercurius')
+const { PrismaClient } = require('@prisma/client')
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+const schema = `
+  type Post {
+    id: Int
+    title: String
+  }
+  type Query {
+    getPosts: [Post]
+    searchPosts(term: String): [Post]
+  }
+  type Mutation {
+    post(title: String!): Post
+  }
+`
+
+const resolvers = {
+  Query: {
+    getPosts: async (_parent, args, context) => {
+      return context.prisma.post.findMany();
+    },
+    searchPosts: async (_parent, args, context) => {
+      const result = await prisma.post.findMany({
+        where: { title: args.term },
+      })
+      return result;
+    }
+  },
+  Mutation: {
+    post: async (_parent, args, context) => {
+      return context.prisma.post.create({
+        data: {
+          title: args.title
+        }
+      })
+    },
+  }
+}
+
+async function createServer() {
+  await app.register(mercurius, {
+    schema,
+    resolvers,
+    context: (request, reply) => {
+      return { prisma }
+    },
+    graphiql: true,
+  })
+
+  await app.register(cors, {
+    origin: 'http://localhost:3000'
+  })
+
+  await app.listen(3001)
+    .then(() => console.log(`🚀 Server ready at http://localhost:3001/graphiql`))
+
+  return app
+}
+
+createServer();
